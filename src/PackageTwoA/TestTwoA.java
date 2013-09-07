1063cb3b2ddd712d26aedd534e86bac310256a9b
dolematt <dolematt@grinnell.edu>
2013-09-07 15:23:14 -0500

    Tests for first 5 procedures

--- src/PackageTwoA/TestTwoA.java
+++ src/PackageTwoA/TestTwoA.java
@@ -6,53 +6,72 @@ import org.junit.Test;
 
 public class TestTwoA {
 
-    @Test
-    public void test_isMultiple(void) {
-	for(int i=1; i<50; i++)
-	{
-	    assertEquals("a is greater than b", false, TwoA.isMultiple(50, i));
-	}
-	for(int i=1; i<50; i++)
-	{
-	    assertEquals("basic functionality", true, TwoA.isMultiple((2 * i), 2));
-	}
-	assertEquals("a is 0", true, TwoA.isMultiple(0, 5));
-	assertEquals("b is 0", false, TwoA.isMultiple(5, 0));
-	assertEquals("both are 0", true, TwoA.isMultiple(0, 0));
-    }
+//    @Test
+//    public void test_isMultiple() {
+//	for(long i=1; i<51; i++)
+//	{
+//	    assertEquals("b is greater than a", false, TwoA.isMultiple(i, 51));
+//	}
+//	for(long i=Integer.MIN_VALUE; i<Integer.MAX_VALUE; i++)
+//	{
+//	    assertEquals("values of a between the min and max values (including 0)", true, TwoA.isMultiple((2 * i), 2));
+//	}
+//	for(long i=Integer.MIN_VALUE; i<Integer.MAX_VALUE; i++)
+//	{
+//	    assertEquals("values of b between the min and max values (including 0)", true, TwoA.isMultiple((4 * i), (2 *i)));
+//	}
+//	assertEquals("b is 0 and a is not", false, TwoA.isMultiple(5, 0));
+//    }
 
-    /*
+
     @Test
-    public void test_isOdd(void) {
+    public void test_isOdd() {
 	assertEquals("negative even", false, (TwoA.isOdd(-6)));
 	assertEquals("negative odd", true, (TwoA.isOdd(-5)));
 	assertEquals("positive even", false, (TwoA.isOdd(6)));
 	assertEquals("positive odd", true, (TwoA.isOdd(5)));
 	assertEquals("zero", false, (TwoA.isOdd(0)));
-	assertEquals("MAX_VALUE", true, TwoA.isOdd(Integer.MAX_VALUE));
+	assertEquals("100,000", false, TwoA.isOdd(100000));
     }
+
+    
     @Test
-    public void test_oddSumTo(void) {
+    public void test_oddSumTo() {
 	assertEquals("n is negative", 0, TwoA.oddSumTo(-1));
 	assertEquals("n is 1", 0, TwoA.oddSumTo(1));
 	assertEquals("n is a positive even", 25, TwoA.oddSumTo(10));
 	assertEquals("n is a positive odd", 16, TwoA.oddSumTo(9));
     }
+
+    
     @Test
-    public void test_isOddProd(void) {
-	assertEquals("all numbers odd", true, TwoA.isOddProd([1, 3, 5])));    
-	assertEquals("all numbers even", false, TwoA.isOddProd([2, 4, 6]));
-	assertEquals("evens, one odd", false, TwoA.isOddProd([2, 4, 6, 1]));
-	assertEquals("evens, two or more odds", true, TwoA.isOddProd([2, 4, 6, 1, 3]));
+    public void test_isOddProd() {
+	int [] allNumOdd = { 1, 3, 5};
+	int [] allNumEven = { 2, 4, 6};
+	int [] evens1Odd = { 2, 4, 6, 1};
+	int [] evens2Odd = { 2, 4, 6, 1, 3};
+	assertEquals("all numbers odd", true, TwoA.isOddProd(allNumOdd));    
+	assertEquals("all numbers even", false, TwoA.isOddProd(allNumEven));
+	assertEquals("evens, one odd", false, TwoA.isOddProd(evens1Odd));
+	assertEquals("evens, two or more odds", true, TwoA.isOddProd(evens2Odd));
     }
+
+    
     @Test
-    public void test_allDistinct(void) {
-	assertEquals("all distinct", true, TwoA.allDistinct([1, 2, -3]))
-	assertEquals("some distinct", false, TwoA.allDistinct([-1, 2, 3, 2]))
-	assertEquals("none distinct", false, TwoA.allDistinct([2, 2, 2]))
-	assertEquals("one", true, TwoA.allDistinct([1]))
-	assertEquals("empty", false, TwoA.allDistinct([]))
+    public void test_allDistinct() {
+	int [] allDist = {1, 2, -3};
+	int [] someDist = {-1, 2, 3, 2};
+	int [] noneDist = {2, 2, 2};
+	int [] single = {1};
+	int [] empty = {};
+	assertEquals("all distinct", true, TwoA.allDistinct(allDist));
+	assertEquals("some distinct", false, TwoA.allDistinct(someDist));
+	assertEquals("none distinct", false, TwoA.allDistinct(noneDist));
+	assertEquals("one", true, TwoA.allDistinct(single));
+	assertEquals("empty", true, TwoA.allDistinct(empty));
     }
+}
+    /*
     @Test
     public void test_reverseInts(void) {
 	assertEquals("multiple", [3, 2, 1], TwoA.allDistinct([1, 2, 3]))    
@@ -60,3 +79,4 @@ public class TestTwoA {
 	assertEquals("empty", [], TwoA.allDistinct([]))    
     }
 }
+    */
