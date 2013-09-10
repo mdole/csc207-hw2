8d9e567691660bc887f627b33ef86c74edd55073
dolematt <dolematt@grinnell.edu>
2013-09-09 20:14:49 -0500

    de-commented RationalNumbers to facilitate working on gitHub

--- src/PackageTwoA/RationalNumbers.java
+++ src/PackageTwoA/RationalNumbers.java
@@ -1,169 +1,166 @@
-///**package PackageTwoA;
-//
-//public class RationalNumbers {
-//
-//    /**
-//     * @param args
-//     * get numerator of rational #
-//     * multiply rational number by an integer
-//     * get denominator of rational #
-//     * multiply two rationals together
-//     * add two rationals together
-//     * find the reciprocal
-//     * find the absolute value
-//     * raise to exponent
-//     * raise numerator to exponent
-//     * raise denomenator to exponent
-//     * raise a number to a rational exponent
-//     * take the log of a rational number
-//     * apply trigonometric functions to rational numbers
-//     * round a rational number
-//     * floor a rational number
-//     * ceiling a rational number
-//     * modulate rational numbers
-//     */
-//    
-//    /**
-//     * Does something (Observer)
-//     **/
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    
-//
-//}
-//**/
+/**package PackageTwoA;
+
+public class RationalNumbers {
+
+    /**
+     * @param args
+     * get numerator of rational #
+     * multiply rational number by an integer
+     * get denominator of rational #
+     * multiply two rationals together
+     * add two rationals together
+     * find the reciprocal
+     * find the absolute value
+     * raise to exponent
+     * raise numerator to exponent
+     * raise denomenator to exponent
+     * raise a number to a rational exponent
+     * round a rational number
+     * floor a rational number
+     * ceiling a rational number
+     */
+    
+    /**
+     * Does something (Observer)
+     **/
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    /**
+     * Does something (Observer)
+     */
+    public static double main() {
+	return 0; //STUB
+
+    }
+    
+
+}
+**/
