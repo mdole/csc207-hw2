cebd6bea1b7ee47c1ce9143d11760b6ff9084fe0
m13hall <hallmira@grinnell.edu>
2013-09-09 20:45:43 -0500

    Added stubs for getNum through absVal

--- src/PackageTwoA/RationalNumbers.java
+++ src/PackageTwoA/RationalNumbers.java
@@ -1,169 +1,78 @@
-///**package PackageTwoA;
-//
-//public class RationalNumbers {
-//
-//    /**
-//     * @param args
-//     * get numerator of rational #
-//     * multiply rational number by an integer
-//     * get denominator of rational #
-//     * multiply two rationals together
-//     * add two rationals together
-//     * find the reciprocal
-//     * find the absolute value
-//     * raise to exponent
-//     * raise numerator to exponent
-//     * raise denomenator to exponent
-//     * raise a number to a rational exponent
-//     * take the log of a rational number
-//     * apply trigonometric functions to rational numbers
-//     * round a rational number
-//     * floor a rational number
-//     * ceiling a rational number
-//     * modulate rational numbers
-//     */
-//    
-//    /**
-//     * Does something (Observer)
-//     **/
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    /**
-//     * Does something (Observer)
-//     */
-//    public static double main() {
-//	return 0; //STUB
-//
-//    }
-//    
-//
-//}
-//**/
+/**package PackageTwoA;
+
+public class RationalNumbers {
+
+    /**
+     * @param args
+     * get numerator of rational # *
+     * get denominator of rational #
+     * multiply two rationals together
+     * add two rationals together
+     * find the reciprocal
+     * find the absolute value*
+     * raise numerator to exponent
+     * raise denomenator to exponent
+     * raise a number to a rational exponent
+     * take the log of a rational number
+     * apply trigonometric functions to rational numbers
+     * round a rational number
+     * floor a rational number
+     * ceiling a rational number
+     * modulate rational numbers
+     */
+    
+    /**
+     * Gets the numerator of a rational number
+     * Observer
+     **/
+    public static double getNum() {
+	return 0; //STUB
+
+    }
+    /**
+     * Gets the denominator of a rational number
+     * Observer
+     */
+    public static double getDenom() {
+	return 0; //STUB
+
+    }
+    /**
+     * multiplies this rational number by another rational number     * takes two rational numbers
+     * returns their product 
+     * Observer/Constructor
+     */
+    public static Rational multiply(double multiplicand) {
+	return null; //STUB
+
+    }
+    /**
+     * adds this rational number to another rational number
+     * returns their sum
+     * (Observer/Constructor)
+     * 
+     */
+    public static Rational add(double additor) {
+	return null; //STUB
+
+    }
+    
+    /**
+     * Finds the reciporcal of this rational number 
+     * Mutator
+     */
+    public static void reciprocal() {
+	return null; //STUB
+    }
+    
+    /**
+     * Finds the absolute value of this rational number
+     * Mutator
+     */
+    public static void absVal() {
+	return null; //STUB
+
+    }
+    
+}
+**/
