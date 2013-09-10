560fe6d654da94ad5b0bc407f4711995af5da75e
dolematt <dolematt@grinnell.edu>
2013-09-09 22:04:09 -0500

    finished rational number testing and stubs

--- src/PackageTwoA/RationalNumbers.java
+++ src/PackageTwoA/RationalNumbers.java
@@ -1,114 +1,97 @@
-/**package PackageTwoA;
+package PackageTwoA;
 
 public class RationalNumbers {
 
-    /**
-     * @param args
-     * get numerator of rational #
-     * multiply rational number by an integer
-     * get denominator of rational #
-     * multiply two rationals together
-     * add two rationals together
-     * find the reciprocal
-     * find the absolute value
-     * raise to exponent
-     * raise numerator to exponent
-     * raise denomenator to exponent
-     * raise a number to a rational exponent
-     * round a rational number
-     * floor a rational number
-     * ceiling a rational number
-     */
-    
-    /**
-     * Does something (Observer)
+    static double negativeLess = -.5;
+    static double negativeGreater = -1.5;
+    static double positiveGreater = 1.5;
+    static double positiveLess = .5;
+    static double zero = 0.0;
+
+    /**Gets the numerator of a rational number
+     * Observer
      **/
-    public static double main() {
+    public static int getNum() {
 	return 0; //STUB
+    }//getNum
 
-    }
     /**
-     * Does something (Observer)
+     * Gets the denominator of a rational number
+     * Observer
      */
-    public static double main() {
+    public static double getDenom() {
 	return 0; //STUB
+    }//getDenom
 
-    }
     /**
-     * Does something (Observer)
+     * multiplies this rational number by another rational number * takes two rational numbers
+     * returns their product
+     * Observer/Constructor
      */
-    public static double main() {
-	return 0; //STUB
+    public static Rational multiply(double multiplicand) {
+	return null; //STUB
+    }//multiply
 
-    }
     /**
-     * Does something (Observer)
+     * adds this rational number to another rational number
+     * returns their sum
+     * (Observer/Constructor)
+     *
      */
-    public static double main() {
-	return 0; //STUB
+    public static Rational add(double additor) {
+	return null; //STUB
+    }//add
 
-    }
     /**
-     * Does something (Observer)
+     * Finds the reciporcal of this rational number
+     * Mutator
      */
-    public static double main() {
-	return 0; //STUB
+    public static void reciprocal() {
+	return null; //STUB
+    }//reciprocal
 
-    }
     /**
-     * Does something (Observer)
+     * Finds the absolute value of this rational number
+     * Mutator
      */
-    public static double main() {
-	return 0; //STUB
+    public static void absVal() {
+	return null; //STUB
+    }//absVal
 
-    }
-    /**
-     * Does something (Observer)
-     */
-    public static double main() {
-	return 0; //STUB
-
-    }
-     
     /**
      * Raise the numerator of a rational number to an exponent (Mutator)
      */
     public static void numeratorExponent(double exponent) {
-	return 0;		//STUB
+	return 0;//STUB
     } //numeratorExponent
-    
+
     /**
-     * Raise the denomenator of a rational number to an exponent (Mutator)
+     * Raise the denominator of a rational number to an exponent (Mutator)
      */
-    public static void denomenatorExponent(double exponent) {
-	return 0; 		//STUB
+    public static void denominatorExponent(double exponent) {
+	return 0; //STUB
     }//denomenatorExponent
-    
-    /**
-     * Raise a number to a rational exponent (Mutator)
-     */
-    public static void rationalExponent(double base) {
-	return 0; 		//STUB
-    }//rationalExponent
-    
+
+
     /**
      * Round a rational number to the nearest whole (Mutator)
      */
-    public static void rationalRound() {
-	return 0; 		//STUB
+    public static void roundRational() {
+	return 0; //STUB
     }//rationalRound
-    
+
+
     /**
      * Floor a rational number (round down to the nearest integer) (Mutator)
      */
     public static void floorRational() {
-	return 0; 		//STUB
+	return 0; //STUB
     }//floorRational
-    
+
     /**
      * Ceiling a rational number (round it up to the nearest integer) (Mutator)
      */
     public static void ceilingRational() {
-	return 0; 		//STUB
+	return 0; //STUB
     }//ceilingRational
 }
